2f45131940f3597ca63f460505d66b881d09fa4e
Maiten Blanc <maitengblanc@gmail.com>
2026-03-21 12:28:32 -0300

    Añadir modificación 1 a factorial.py: si no se le pasa argumentos a la función

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python
-#*-------------------------------------------------------------------------*
-#* factorial.py                                                            *
-#* calcula el factorial de un número                                       *
-#* Dr.P.E.Colla (c) 2022                                                   *
-#* Creative commons                                                        *
-#*-------------------------------------------------------------------------*
+# *-------------------------------------------------------------------------*
+# * factorial.py                                                            *
+# * calcula el factorial de un número                                       *
+# * Dr.P.E.Colla (c) 2022                                                   *
+# * Creative commons                                                        *
+# *-------------------------------------------------------------------------*
 import sys
-def factorial(num): 
-    if num < 0: 
+
+
+def factorial(num):
+    if num < 0:
         print("Factorial de un número negativo no existe")
         return 0
-    elif num == 0: 
+    elif num == 0:
         return 1
-        
-    else: 
+
+    else:
         fact = 1
-        while(num > 1): 
-            fact *= num 
+        while num > 1:
+            fact *= num
             num -= 1
-        return fact 
+        return fact
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+# Modificación 1: Si no se pasa argumento
+if len(sys.argv) < 2:
+    num = input("Debe informar un número: ")
+else:
+    num = sys.argv[1]
 
+num = int(num)
+print("Factorial ", num, "! es ", factorial(num))
+# Fin modificación 1
