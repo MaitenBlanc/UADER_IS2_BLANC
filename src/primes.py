91f7003f3bb475fb03634221eb5e42e097f3a32d
Maiten Blanc <maitengblanc@gmail.com>
2026-03-21 11:03:41 -0300

    agregar comentarios a src/primes.py

--- src/primes.py
+++ src/primes.py
@@ -4,13 +4,15 @@
 lower = 1
 upper = 500
 
+# Imprimir en pantalla
 print("Prime numbers between", lower, "and", upper, "are:")
 
+# Ciclo para mostrar todos los números primos
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
-   if num > 1:
-       for i in range(2, num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num)
+    # all prime numbers are greater than 1
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
