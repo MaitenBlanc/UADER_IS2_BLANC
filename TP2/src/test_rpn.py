5d87a5b1b74121343eec6480bb534c36af16e0ab
Maiten Blanc <maitengblanc@gmail.com>
2026-04-08 12:50:20 -0300

    Version 1.1 test_rpn.py - Coverage = 88 %

--- TP2/src/test_rpn.py
+++ TP2/src/test_rpn.py
@@ -6,109 +6,112 @@ import math
 class TestRPN(unittest.TestCase):
 
     # ------------------------
-    # OPERACIONES BÁSICAS
+    # CASOS CORRECTOS
     # ------------------------
-    def test_suma(self):
+    def test_operaciones(self):
         self.assertEqual(eval_rpn("3 4 +".split()), 7)
-
-    def test_expresion_compleja(self):
         self.assertEqual(eval_rpn("5 1 2 + 4 * + 3 -".split()), 14)
-
-    def test_multiplicacion(self):
         self.assertEqual(eval_rpn("2 3 4 * +".split()), 14)
-
-    def test_division(self):
         self.assertEqual(eval_rpn("8 2 /".split()), 4)
 
-    def test_floats(self):
+    def test_floats_y_negativos(self):
         self.assertAlmostEqual(eval_rpn("2.5 2 *".split()), 5.0)
-
-    def test_negativos(self):
         self.assertEqual(eval_rpn("-3 2 *".split()), -6)
 
-    # ------------------------
-    # ERRORES
-    # ------------------------
-    def test_division_por_cero(self):
-        with self.assertRaises(RPNError):
-            eval_rpn("3 0 /".split())
-
-    def test_token_invalido(self):
-        with self.assertRaises(RPNError):
-            eval_rpn("2 3 &".split())
-
-    def test_pila_insuficiente(self):
-        with self.assertRaises(RPNError):
-            eval_rpn("+".split())
-
-    def test_pila_final_invalida(self):
-        with self.assertRaises(RPNError):
-            eval_rpn("2 3".split())
-
-    # ------------------------
-    # FUNCIONES
-    # ------------------------
-    def test_sqrt(self):
+    def test_funciones(self):
         self.assertEqual(eval_rpn("9 sqrt".split()), 3)
-
-    def test_log(self):
         self.assertEqual(eval_rpn("100 log".split()), 2)
-
-    def test_ln(self):
         self.assertAlmostEqual(eval_rpn("2.718281828 ln".split()), 1, places=3)
-
-    def test_ex(self):
-        self.assertAlmostEqual(eval_rpn("1 ex".split()), math.e)
-
-    def test_potencia(self):
         self.assertEqual(eval_rpn("2 3 yx".split()), 8)
-
-    def test_inverso(self):
         self.assertEqual(eval_rpn("2 1/x".split()), 0.5)
-
-    def test_10x(self):
         self.assertEqual(eval_rpn("2 10x".split()), 100)
 
-    def test_chs(self):
-        self.assertEqual(eval_rpn("5 chs".split()), -5)
-
-    # ------------------------
-    # TRIGONOMETRÍA
-    # ------------------------
-    def test_sin(self):
+    def test_trig(self):
         self.assertAlmostEqual(eval_rpn("90 sin".split()), 1, places=5)
-
-    def test_cos(self):
         self.assertAlmostEqual(eval_rpn("0 cos".split()), 1, places=5)
-
-    def test_tg(self):
         self.assertAlmostEqual(eval_rpn("45 tg".split()), 1, places=5)
 
-    # ------------------------
-    # PILA
-    # ------------------------
-    def test_dup(self):
+    def test_pila(self):
         self.assertEqual(eval_rpn("3 dup *".split()), 9)
-
-    def test_swap(self):
         self.assertEqual(eval_rpn("3 4 swap -".split()), 1)
-
-    def test_drop(self):
         self.assertEqual(eval_rpn("3 4 drop".split()), 3)
 
-    def test_clear_error(self):
-        with self.assertRaises(RPNError):
-            eval_rpn("3 4 clear".split())
-
-    # ------------------------
-    # MEMORIA
-    # ------------------------
-    def test_sto_rcl(self):
+    def test_memoria(self):
         self.assertEqual(eval_rpn("5 01 sto 01 rcl".split()), 5)
 
+    # ------------------------
+    # ERRORES (CON TRY/EXCEPT)
+    # ------------------------
+
+    def test_division_por_cero(self):
+        try:
+            eval_rpn("3 0 /".split())
+            self.fail("Se esperaba excepción por división por cero")
+        except RPNError as e:
+            self.assertIn("cero", str(e).lower())
+
+    def test_token_invalido(self):
+        try:
+            eval_rpn("2 3 &".split())
+            self.fail("Se esperaba excepción por token inválido")
+        except RPNError as e:
+            self.assertIn("token", str(e).lower())
+
+    def test_pila_insuficiente(self):
+        try:
+            eval_rpn("+".split())
+            self.fail("Se esperaba excepción por pila insuficiente")
+        except RPNError as e:
+            self.assertIn("pila", str(e).lower())
+
+    def test_pila_final_invalida(self):
+        try:
+            eval_rpn("2 3".split())
+            self.fail("Se esperaba error por pila final inválida")
+        except RPNError as e:
+            self.assertIn("exactamente un valor", str(e).lower())
+
+    def test_sqrt_negativo(self):
+        try:
+            eval_rpn("-1 sqrt".split())
+            self.fail("Se esperaba error de raíz negativa")
+        except RPNError as e:
+            self.assertIn("negativa", str(e).lower())
+
+    def test_log_invalido(self):
+        try:
+            eval_rpn("0 log".split())
+            self.fail("Se esperaba error log inválido")
+        except RPNError as e:
+            self.assertIn("log", str(e).lower())
+
+    def test_ln_invalido(self):
+        try:
+            eval_rpn("-1 ln".split())
+            self.fail("Se esperaba error ln inválido")
+        except RPNError as e:
+            self.assertIn("ln", str(e).lower())
+
+    def test_swap_insuficiente(self):
+        try:
+            eval_rpn("1 swap".split())
+            self.fail("Se esperaba error en swap")
+        except RPNError as e:
+            self.assertIn("pila", str(e).lower())
+
     def test_memoria_invalida(self):
-        with self.assertRaises(RPNError):
+        try:
             eval_rpn("5 10 sto".split())
+            self.fail("Se esperaba error de memoria inválida")
+        except RPNError as e:
+            self.assertIn("memoria", str(e).lower())
+
+    def test_clear_pila_final(self):
+        try:
+            eval_rpn("3 4 clear".split())
+            self.fail("Se esperaba error por pila vacía al final")
+        except RPNError as e:
+            self.assertIn("exactamente un valor", str(e).lower())
 
 
 if __name__ == "__main__":
